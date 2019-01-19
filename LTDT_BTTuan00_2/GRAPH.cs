41d570f11744a64c18b053d5bfad39e66f727eeb
Joy Trinh <mongtuyen149@gmail.com>
2019-01-19 11:58:47 +0700

    First commit

---
+++ LTDT_BTTuan00_2/GRAPH.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTDT_BTTuan00_2
+{
+    class GRAPH
+    {
+        private int _numberOfVertexes;
+        private int[,] _matrix;
+
+        public int numberOfVertexes {
+            get { return _numberOfVertexes; }
+            set
+            {
+                if (value >= 0)
+                    _numberOfVertexes = value;
+            }
+        }
+
+        public int[,] matrix
+        {
+            get { return _matrix; }
+            set { _matrix = value; }
+        }
+    }
+}
---
+++ LTDT_BTTuan00_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTDT_BTTuan00_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
